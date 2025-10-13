a86d3160eb9fb78ee00e034cd6b6f9b3ecfeff28
pravin-kavthale <kavthalepravin@gmail.com>
2025-10-13 23:24:36 +0530

    Add Register User Form

--- Users/views.py
+++ Users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from .forms import UserRegisterForm
 # Create your views here.
+
+def register(request):
+    if request.method=="POST":
+        form=UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form=UserRegisterForm()
+    return render(request,'Users/register.html',{'form':form})
